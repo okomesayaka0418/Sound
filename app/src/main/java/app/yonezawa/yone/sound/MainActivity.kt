e6c3d452462f47c1312a7ef75a290412620ba955
lifeistech <hoshi418saya@gmail.com>
2021-05-08 02:04:12 +0900

    サウンドアプリ2の完成

--- app/src/main/java/app/yonezawa/yone/sound/MainActivity.kt
+++ app/src/main/java/app/yonezawa/yone/sound/MainActivity.kt
@@ -4,6 +4,7 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,15 +15,31 @@ class MainActivity : AppCompatActivity() {
         //ドラムのサウンドファイルを読み込んで、プレイヤーを作る
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
 
-        //ドラムがタップされたときに呼ばれる
-        drumImage.setOnClickListener {
+        //ドラムがタッチされたときに呼ばれる
+        drumImage.setOnTouchListener { view, event ->
 
-            //ドラムの音を巻き戻す
-            drumSound.seekTo(0)
+            //タッチが始まった時に
+            if (event.action == MotionEvent.ACTION_DOWN) {
 
-            //ドラムの音を再生する
-            drumSound.start()
+                //ドラムが鳴っている画像に切り替える
+                drumImage.setImageResource(R.drawable.drum_playing_image)
+
+                //ドラムの音を巻き戻す
+                drumSound.seekTo(0)
+
+                //ドラムの音を再生する
+                drumSound.start()
+
+            }
+            //タッチが終わるときに
+            else if (event.action == MotionEvent.ACTION_UP){
+
+                //ドラムが鳴っていない画像に切り替える
+                drumImage.setImageResource(R.drawable.drum_image)
+
+
+            }
+            true
         }
     }
-
 }
